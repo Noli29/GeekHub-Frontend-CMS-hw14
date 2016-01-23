2784e81f028004d2d22043851fdabeab14e8599c
Noli29 <noli.pogorelova@gmail.com>
2016-01-23 19:51:03 +0200

    level5

--- hw.js
+++ hw.js
@@ -30,4 +30,42 @@ for(i = 0; i < 14; i++){
 }
 
 //Level 5
+#########
+#@      #
+####### #
+#   #   #
+# # # ###
+# # # ###
+# #   ###
+# #######
+#      *#
+#########
+
 map();
+
+var i;
+for(i=0; i<10; i++){
+	if (isFree('east')==true) 
+		east();
+	else
+		south();
+}
+for (i=0; i<6; i++){
+	if (isFree('west')==true)
+		west();
+	else
+		south();
+}
+for (i=0; i<10; i++){
+	if (isFree('west')==true)
+		west();
+	else
+		north();
+}
+for (i=0; i<11; i++){
+	if (isFree('south')==true)
+		south();
+	else
+		east();
+}
+		
